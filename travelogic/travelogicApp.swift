eb2d3542d1e44a5652e7bc5c0db0a5d2f0459647
Chean <cheanzcg@gmail.com>
2025-09-03 14:13:18 -0700

    Initial commit

---
+++ travelogic/travelogicApp.swift
@@ -0,0 +1,32 @@
+//
+//  travelogicApp.swift
+//  travelogic
+//
+//  Created by admin on 9/3/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct travelogicApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
